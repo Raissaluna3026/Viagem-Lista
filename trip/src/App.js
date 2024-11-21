c829f6c42be204b7bef5c6ca81e923962b521432
Raissa Luna <108363024+Raissaluna3026@users.noreply.github.com>
2024-11-21 02:10:10 -0300

    componentes iniciais

--- trip/src/App.js
+++ trip/src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  {id:1, description: "Passaporte", quantity:1, packed:true},
+  {id:2, description: "Meias", quantity:2, packed:true},
+  {id:3, description: "Carregador", quantity:1, packed:false},
+]
+
+
 export default function App(){
   return (
     <div className="app">
@@ -18,15 +25,50 @@ function Logo(){
 }
 
 function Form(){
-  return <div className="add-form">
-    <h3>
-      O que você precisa por na mala para sua viagem?
-    </h3>
-  </div>
+  function handleSubmit(e){
+    e.preventDefault();
+    
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>
+        O que você precisa por na mala para sua viagem?
+      </h3>
+      <select>
+        {Array.from({length:20}, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+    <input type='text' placeholder="Item..."/>
+    <button onClic={handleSubmit}>Adicionar</button>
+  </form>
+  )
 }
 
 function ListaDeCoisas(){
-  return <div className="list">LISTA</div>
+  return (
+    <div className="list">
+      <ul >
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id}/>  
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+function Item({item}){
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+      {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  )
 }
 
 function Estatistica(){

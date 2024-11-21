522d62b725673840f186f61bdbb02881edf5bc62
Raissa Luna <108363024+Raissaluna3026@users.noreply.github.com>
2024-11-21 16:21:38 -0300

    funçoes para renderizaçao com os hooks

--- trip/src/App.js
+++ trip/src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 const initialItems = [
   {id:1, description: "Passaporte", quantity:1, packed:true},
   {id:2, description: "Meias", quantity:2, packed:true},
@@ -23,27 +25,41 @@ function Logo(){
     </h1>
   )
 }
-
+  
 function Form(){
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(5);
+
   function handleSubmit(e){
     e.preventDefault();
     
+    if (!description) return;
+    const newItem = {description,quantity,packed:false, id: Date.now()};
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+
   }
 
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit} onChange={(e) => setQuantity(Number(e.target.value))}>
       <h3>
         O que você precisa por na mala para sua viagem?
       </h3>
-      <select>
+      <select value={quantity}>
         {Array.from({length:20}, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-    <input type='text' placeholder="Item..."/>
-    <button onClic={handleSubmit}>Adicionar</button>
+    <input 
+    type='text' 
+    placeholder="Item..." 
+    value={description} 
+    onChange={(e) => setDescription(e.target.value)
+    } />
+    <button onClick={handleSubmit}>Adicionar</button>
   </form>
   )
 }

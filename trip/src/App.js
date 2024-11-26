af38c13337d9b61fa3458f6c6a9be5456e01ac10
Raissa Luna <108363024+Raissaluna3026@users.noreply.github.com>
2024-11-26 00:10:53 -0300

    botão de limpar lista

--- trip/src/App.js
+++ trip/src/App.js
@@ -24,6 +24,11 @@ export default function App(){
       item.id === id ? {...item, packed: !item.packed}
      : item))
   }
+
+  function deleteItems(){
+    setItems([]);
+
+  }
     
   
 
@@ -31,7 +36,7 @@ export default function App(){
     <div className="app">
       <Logo/>
       <Form onAddItems={handleAddItems}/>
-      <ListaDeCoisas items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
+      <ListaDeCoisas items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onDeleteItems={deleteItems}/>
       <Estatistica items={items}/>
     </div>
   )
@@ -85,14 +90,33 @@ function Form({onAddItems}){
   )
 }
 
-function ListaDeCoisas({items, onDeleteItem, onToggleItem}){
+function ListaDeCoisas({items, onDeleteItem, onToggleItem, onDeleteItems}){
+  const [sortBy, setSortBy] = useState('packed');
+
+  let sortedItems;
+
+  if(sortBy === 'input') sortedItems = items;
+  if(sortBy === 'description') 
+    sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+
+  if(sortBy === 'packed') 
+    sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul >
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item item={item} onDeleteItem={onDeleteItem} key={item.id} onToggleItem={onToggleItem}/>  
         ))}
       </ul>
+      <div className='actions'>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>Classificar por ordem de entrada</option>
+          <option value='description'>Classificar por descrição</option>
+          <option value='packed'>Classificar por status da embalagem</option>
+        </select>
+        <button onClick={() => onDeleteItems()}>Limpar lista</button>
+      </div>
     </div>
   )
 }
